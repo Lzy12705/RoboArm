98179e63181999f70b0dc4ea7961f3ed8df9d31e
Leezy <zhuoy.li@mail.utoronto.ca>
2026-07-07 00:27:24 -0400

    Finished building the main logic, td3_torch with agent training, and buffer for store_transition.

---
+++ tmp/test.py
@@ -0,0 +1,38 @@
+import os
+import gym
+import numpy as np
+from torch.utils.tensorboard import SummaryWriter
+import robosuite as suite
+from robosuite.wrappers import GymWrapper
+from td3_torch import Agent
+
+if __name__ == '__main__':
+    if not os.path.exists("tmp/td3"):
+        os.makedirs("tmp/td3")
+    
+    env_name = "Door"
+    
+    env = suite.make(
+        env_name,
+        robots=["Panda"],
+        controller_configs = suite.load_part_controller_config(default_controller="JOINT_VELOCITY")
+        has_renderer = True,
+        use_camera_abs = False,
+        horizon = 300,
+        render_camera = "frontview"
+        has_offscreen_renderer = True,
+        control_freq = 20,
+    )
+    
+    env = GymWrapper(env)
+    
+    actor_learning_rate = 0.001
+    critic_learning_rate = 0.001
+    batch_size = 128
+    layer1_size = 256
+    layer2_size = 128
+    
+    agent = Agent(actor_learning_rate = actor_learning_rate, critic_learning_rate = critic_learning_rate, tau = 0.005, input_dims = env.observation_space.shape,
+                  env = env, n_actions = env.action_space.shape[0], layer1_size = layer1_size, layer2_size = layer2_size, batch_size = batch_size)
+    
+    n_games = 
